7cf79b704db459443a1c54c4be2f204729b58912
Himansyou <himanshurawat2k4@gmail.com>
2026-03-29 21:49:51 +0530

    Fixed some errors and is workable but still in initial phase

--- CLI/build.gradle.kts
+++ CLI/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     application
+    id("org.graalvm.buildtools.native") version "0.10.2"
 }
 
 group = "org.himansyou"
@@ -12,6 +13,8 @@ repositories {
 
 dependencies {
     implementation("info.picocli:picocli:4.7.6")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.6") // 👈 ADD THIS
+
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.20.1")
     implementation("org.xerial:sqlite-jdbc:3.49.1.0")
@@ -20,13 +23,25 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.24.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
 
+application {
+    mainClass.set("org.himansyou.Main")
+}
+
+graalvmNative {
+    binaries {
+        named("main") {
+            imageName.set("freecli")
+        }
+    }
 }
 
 tasks.test {
     useJUnitPlatform()
 }
-application {
-    mainClass.set("org.himansyou.Main")
+tasks.withType<JavaCompile> {
+    options.compilerArgs.addAll(
+        listOf("-Aproject=${project.group}/${project.name}")
+    )
 }
-
